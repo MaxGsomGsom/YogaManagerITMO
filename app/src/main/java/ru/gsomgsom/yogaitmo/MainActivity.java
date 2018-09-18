8fa81a0661f259ea1ab2ad729c0e9deb8d5dcedb
Max Kuzmin <max_kuzmin@outlook.com>
2018-09-18 14:13:15 +0300

    Fix

--- app/src/main/java/ru/gsomgsom/yogaitmo/MainActivity.java
+++ app/src/main/java/ru/gsomgsom/yogaitmo/MainActivity.java
@@ -8,13 +8,12 @@ import android.webkit.WebResourceRequest;
 
 public class MainActivity extends Activity {
 
-    int timeOut = 2000;
     String HideWarnings = " $('div.note-warning, div.note-danger').hide(); ";
     String HideColumns = " $('div#attendance tr').each(function(index) { $('th, td', this).slice(1).hide(); }); ";
-    String OnLessonChanged = " setTimeout(function() { $('select#P6_DATE').empty(); $('div#attendance th').slice(1).each(function(index) { $('select#P6_DATE').append(\"<option value='\" + index + \"'>\" + $(this).text() + \"</option>\"); " + HideWarnings + HideColumns + " }); }, " + timeOut + "); ";
+    String RefreshStatementFunc = " window.refresh_statement = function() { $('select#P6_DATE').empty(); $('div#attendance th').slice(1).each(function(index) { $('select#P6_DATE').append(\"<option value='\" + index + \"'>\" + $(this).text() + \"</option>\"); " + HideWarnings + HideColumns + " }); }; ";
     String OnDateChanged = HideColumns + " var colNum = parseInt($('select#P6_DATE').val()) + 1; $('div#attendance tr').each(function(index) { $('th:eq(' + colNum + ')', this).show(); $('td:eq(' + colNum + ')', this).show(); }); ";
     String AppendSelectNode =" $('div#R1294308328580210889 div.panel-body').append(\"<div class='form-group'><label for='P6_DATE' class='control-label optional' tabindex='999'>Дата</label><select id='P6_DATE' class='selectlist form-control' size='1'></select></div>\"); ";
-    String OnLoaded = AppendSelectNode + HideWarnings + " $('select#P6_TEACHER_SELECT_CLASS').change(function() { " + OnLessonChanged + " }); $('select#P6_DATE').change(function() { " + OnDateChanged + " }); ";
+    String OnLoaded = AppendSelectNode + HideWarnings + RefreshStatementFunc + " $('select#P6_DATE').change(function() { " + OnDateChanged + " }); ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +34,6 @@ public class MainActivity extends Activity {
         });
         web.getSettings().setJavaScriptEnabled(true);
         web.getSettings().setDomStorageEnabled(true);
-        web.getSettings().setAppCacheEnabled(true);
 
         web.loadUrl("https://isu.ifmo.ru/pls/apex/f?p=2153:6::");
 
